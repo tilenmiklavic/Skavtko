72366b806ae4fb538c65276e40a9173d9bee5943
tilenmiklavic <tm7321@student.uni-lj.si>
2024-02-11 18:40:10 +0100

    fix: button

--- src/components/Buttons/primaryButton.tsx
+++ src/components/Buttons/primaryButton.tsx
@@ -6,7 +6,7 @@ interface NavItemProps {
 
 const PrimaryButton = (props: NavItemProps) => {
   let classnameEnabled =
-    "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800";
+    "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 px-5 py-2.5";
   let classnameDisabled =
     "text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center";
 
