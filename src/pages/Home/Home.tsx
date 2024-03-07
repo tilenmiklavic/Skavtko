09f9f94ea0fba95af8dd0899fa4225cd75907af5
tilenmiklavic <tm7321@student.uni-lj.si>
2024-03-07 23:33:14 +0100

    feat: get method for retrieving sheets

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { getSheet } from "../../services/gsheets";
+
 function Home() {
+  const getData = async () => {
+    console.log("Getting sheet");
+    const result = await getSheet();
+    console.log(result);
+  };
+
+  useEffect(() => {
+    console.log("useEffect");
+    getData();
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
--- src/services/gsheets.tsx
+++ src/services/gsheets.tsx
@@ -32,3 +32,37 @@ export async function writeToSheets(values: string[][]) {
     // Handle network errors
   }
 }
+
+export async function getSheet() {
+  // Assuming `formData` is the data you want to append, structured as needed for your Google Sheet
+
+  const access_token = JSON.parse(localStorage.getItem("auth")!).access_token;
+  const sheet_id = JSON.parse(localStorage.getItem("sheetLink")!).id;
+
+  const formData = {
+    accessToken: access_token,
+    apiKey: process.env.REACT_APP_API_KEY,
+    sheetId: sheet_id,
+  };
+
+  try {
+    const response = await fetch("/api/get", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      return result;
+    } else {
+      console.error("Error from server", response);
+      // Handle server errors or non-OK responses
+    }
+  } catch (error) {
+    console.error("Network error:", error);
+    // Handle network errors
+  }
+}
