0c2fe5a96ceca08dc1b28ddf521f476569bbf7e0
tilenmiklavic <tm7321@student.uni-lj.si>
2024-04-17 10:39:28 +0200

    feat: add privacy policy

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -153,7 +153,7 @@ export default function Settings() {
               </TabsBody>
             </Tabs>
           </div>
-          <div className="flex">
+          <div className="flex flex-col">
             {/* <button
               type="submit"
               className="mt-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -168,6 +168,9 @@ export default function Settings() {
             >
               <span>Shrani</span>
             </Button>
+            <a className="w-full" href="/privacy">
+              Privacy policy
+            </a>
           </div>
         </div>
       </form>
