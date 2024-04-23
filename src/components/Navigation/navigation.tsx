49cf4bc2f5ec65bb7b01de074d074ee82a94bcbc
tilenmiklavic <tm7321@student.uni-lj.si>
2024-04-23 17:08:28 +0200

    feat: navigation bar new look

--- src/components/Navigation/navigation.tsx
+++ src/components/Navigation/navigation.tsx
@@ -11,16 +11,41 @@ import "./navigation.css";
 import { faBitcoin } from "@fortawesome/free-brands-svg-icons";
 
 const Navigation = () => {
-  const [tab] = useState(0);
+  const [tab, setTab] = useState(0);
 
   return (
     <nav className="w-full h-20 flex border-b border-t border-gray-200 dark:border-gray-600">
       <div className="w-screen flex justify-around items-center p-4">
-        <NavItem path={"/"} icon={faCheckCircle} enabled={tab === 0} />
-        <NavItem path={"/finance"} icon={faBitcoin} enabled={tab === 1} />
-        <NavItem path={"/on"} icon={faDumbbell} enabled={tab === 2} />
-        <NavItem path={"/statistics"} icon={faChartPie} enabled={tab === 2} />
-        <NavItem path={"/settings"} icon={faGear} enabled={tab === 2} />
+        <NavItem
+          path={"/"}
+          icon={faCheckCircle}
+          enabled={tab === 0}
+          onClick={() => setTab(0)}
+        />
+        <NavItem
+          path={"/finance"}
+          icon={faBitcoin}
+          enabled={tab === 1}
+          onClick={() => setTab(1)}
+        />
+        <NavItem
+          path={"/on"}
+          icon={faDumbbell}
+          enabled={tab === 2}
+          onClick={() => setTab(2)}
+        />
+        <NavItem
+          path={"/statistics"}
+          icon={faChartPie}
+          enabled={tab === 3}
+          onClick={() => setTab(3)}
+        />
+        <NavItem
+          path={"/settings"}
+          icon={faGear}
+          enabled={tab === 4}
+          onClick={() => setTab(4)}
+        />
       </div>
     </nav>
   );
