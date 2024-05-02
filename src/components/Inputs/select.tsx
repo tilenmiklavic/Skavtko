eaaf32c8d5acad21ea770cafd133dfcfc95bdc58
tilenmiklavic <tilen.miklavic@celtra.com>
2024-05-02 11:02:56 +0200

    fix: select component

--- src/components/Inputs/select.tsx
+++ src/components/Inputs/select.tsx
@@ -5,6 +5,7 @@ interface SelectProps {
   id: string;
   placeholder: string;
   value?: string;
+  options: { value: string; label: string }[];
   onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
@@ -24,10 +25,11 @@ const VejaSelect = (props: SelectProps) => {
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       >
         <option>{props.placeholder}</option>
-        <option value="bb">BB</option>
-        <option value="vv">VV</option>
-        <option value="iv">IV</option>
-        <option value="pp">PP</option>
+        {props.options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
       </select>
     </div>
   );
