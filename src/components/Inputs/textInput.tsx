33a5b1338e39b0cdc60a4749d687629a70f0e212
tilenmiklavic <tm7321@student.uni-lj.si>
2024-05-16 23:55:25 +0200

    add: ref element

--- src/components/Inputs/textInput.tsx
+++ src/components/Inputs/textInput.tsx
@@ -1,35 +1,41 @@
-import { on } from "events";
+import React from "react";
 
 interface NavItemProps {
-  label: string;
-  id: string;
-  placeholder: string;
-  required?: boolean;
-  value?: string;
-  defaultValue?: string;
-  disabled?: boolean;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    label?: string;
+    className?: string;
+    id: string;
+    placeholder: string;
+    required?: boolean;
+    value?: string;
+    defaultValue?: string;
+    disabled?: boolean;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const TextInput = (props: NavItemProps) => {
-  return (
-    <div className="mb-3">
-      <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
-        {props.label}
-      </label>
-      <input
-        type="text"
-        id={props.id}
-        name={props.id}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder={props.placeholder}
-        required={props.required}
-        defaultValue={props.defaultValue}
-        onChange={props.onChange}
-        disabled={props.disabled}
-      ></input>
-    </div>
-  );
-};
+const TextInput = React.forwardRef<HTMLInputElement, NavItemProps>(
+    (props, ref) => {
+        return (
+            <div className={props.className}>
+                {props.label && (
+                    <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+                        {props.label}
+                    </label>
+                )}
+                <input
+                    type="text"
+                    id={props.id}
+                    name={props.id}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder={props.placeholder}
+                    required={props.required}
+                    defaultValue={props.defaultValue}
+                    onChange={props.onChange}
+                    disabled={props.disabled}
+                    ref={ref} // Attach the forwarded ref here
+                ></input>
+            </div>
+        );
+    }
+);
 
 export default TextInput;
