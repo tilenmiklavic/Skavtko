fe50cfa0305766e6659d78b6b806f4a7ff99c795
tilenmiklavic <tm7321@student.uni-lj.si>
2024-05-17 11:27:28 +0200

    add: clear button as option to text input

--- src/components/Inputs/textInput.tsx
+++ src/components/Inputs/textInput.tsx
@@ -9,6 +9,7 @@ interface NavItemProps {
     value?: string;
     defaultValue?: string;
     disabled?: boolean;
+    clearButton?: boolean;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
@@ -22,7 +23,7 @@ const TextInput = React.forwardRef<HTMLInputElement, NavItemProps>(
                     </label>
                 )}
                 <input
-                    type="text"
+                    type={props.clearButton ? "search" : "text"}
                     id={props.id}
                     name={props.id}
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
