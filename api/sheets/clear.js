0c3a775e6afa869facb48a3f097f56eb4034053d
tilenmiklavic <tm7321@student.uni-lj.si>
2024-05-18 21:41:34 +0200

    add: new server endpoint for deleting row

--- api/sheets/clear.js
+++ api/sheets/clear.js
@@ -1,37 +1,54 @@
 // /api/sheets/clear.js
 module.exports = async (req, res) => {
-  try {
-    const body = await req.body;
-    const { accessToken, sheetId, apiKey, position } = body;
-
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${position}:clear?key=${apiKey}`;
-    console.log(url);
-
-    const options = {
-      method: "POST",
-      headers: {
-        Authorization: "Bearer " + accessToken,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        valueInputOption: "RAW",
-      },
-      compress: true,
-    };
-
     try {
-      // Fetch data from external API
-      const response = await fetch(url, options);
-      const data = await response.json();
+        const body = await req.body;
+        const { accessToken, sheetId, apiKey, position, method } = body;
+        let url, options;
 
-      console.log(data);
+        if (method === "clear") {
+            url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${position}:clear?key=${apiKey}`;
 
-      res.status(200).json({ data: data });
-      // Handle the response data
+            options = {
+                method: "POST",
+                headers: {
+                    Authorization: "Bearer " + accessToken,
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    valueInputOption: "RAW"
+                },
+                compress: true
+            };
+        } else if (method === "remove_row") {
+            url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values:batchClear?key=${apiKey}`;
+            options = {
+                method: "POST",
+                headers: {
+                    Authorization: "Bearer " + accessToken,
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    ranges: [position]
+                }),
+                compress: true
+            };
+        }
+
+        try {
+            // Fetch data from external API
+            const response = await fetch(url, options);
+            const data = await response.json();
+
+            res.status(200).json({ data: data });
+            // Handle the response data
+        } catch (error) {
+            console.error(
+                "There was a problem with the clear operation:",
+                error
+            );
+        }
     } catch (error) {
-      console.error("There was a problem with the clear operation:", error);
+        console.error(error);
+        res.status(500).send("Error communicating with Google Sheets API");
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error communicating with Google Sheets API");
-  }
 };
