a26a83006e850820232401dcd8c88a6283b0a09f
tilenmiklavic <tm7321@student.uni-lj.si>
2024-05-18 21:42:43 +0200

    fix: confirm dialog looks

--- src/components/Common/ConfirmDialog.tsx
+++ src/components/Common/ConfirmDialog.tsx
@@ -1,51 +1,58 @@
+import { faX, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  Button,
-  Dialog,
-  DialogBody,
-  DialogFooter,
-  DialogHeader,
+    Button,
+    Dialog,
+    DialogBody,
+    DialogFooter,
+    DialogHeader,
+    IconButton
 } from "@material-tailwind/react";
 
 interface DialogProps {
-  open: boolean;
-  title: string;
-  content: string;
-  children?: React.ReactNode;
-  handleConfirm: () => void;
-  handleOpen: () => void;
+    open: boolean;
+    title: string;
+    content?: string;
+    children?: React.ReactNode;
+    handleConfirm: () => void;
+    handleOpen: () => void;
 }
 
 const ConfirmDialog = (props: DialogProps) => {
-  return (
-    <Dialog
-      open={props.open}
-      handler={props.handleOpen}
-      placeholder={undefined}
-    >
-      <DialogHeader placeholder={undefined}>{props.title}</DialogHeader>
-      <DialogBody placeholder={undefined}>{props.content}</DialogBody>
-      {props.children}
-      <DialogFooter placeholder={undefined}>
-        <Button
-          variant="text"
-          color="red"
-          onClick={props.handleOpen}
-          className="mr-1"
-          placeholder={undefined}
+    return (
+        <Dialog
+            open={props.open}
+            handler={props.handleOpen}
+            placeholder={undefined}
         >
-          <span>Nazaj</span>
-        </Button>
-        <Button
-          variant="gradient"
-          color="green"
-          onClick={props.handleConfirm}
-          placeholder={undefined}
-        >
-          <span>Potrdi</span>
-        </Button>
-      </DialogFooter>
-    </Dialog>
-  );
+            <DialogHeader
+                placeholder={undefined}
+                className="flex justify-between"
+            >
+                {props.title}
+                <FontAwesomeIcon
+                    className="icon mr-2"
+                    size="sm"
+                    icon={faXmark}
+                    onClick={props.handleOpen}
+                />
+            </DialogHeader>
+            {props.content && (
+                <DialogBody placeholder={undefined}>{props.content}</DialogBody>
+            )}
+            {props.children}
+            <DialogFooter placeholder={undefined}>
+                <Button
+                    variant="filled"
+                    color="green"
+                    onClick={props.handleConfirm}
+                    placeholder={undefined}
+                >
+                    <span>Potrdi</span>
+                </Button>
+            </DialogFooter>
+        </Dialog>
+    );
 };
 
 export default ConfirmDialog;
