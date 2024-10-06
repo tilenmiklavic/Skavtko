440868ce5901386da87c9232a424ab8e3ccfaacd
tilenmiklavic <tilen.miklavic@celtra.com>
2024-10-06 18:32:12 +0200

    add: checkbox component

---
+++ src/classes/UserNapredovanje.tsx
@@ -0,0 +1,6 @@
+interface UserNapredovanje {
+  Ime: string;
+  Izzivi: number;
+}
+
+export default UserNapredovanje;
---
+++ src/components/Common/ProgressBar.tsx
@@ -0,0 +1,16 @@
+interface ProgressProps {
+  progress: number;
+}
+
+const ProgressBar = (props: ProgressProps) => {
+  return (
+    <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+      <div
+        className="bg-blue-600 h-2.5 rounded-full"
+        style={{ width: `${props.progress}%` }}
+      ></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
