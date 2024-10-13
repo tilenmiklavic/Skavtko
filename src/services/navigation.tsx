e55660d1dee7de32e83294a9cd9189e8187299a7
tilenmiklavic <tilen.miklavic@celtra.com>
2024-10-13 11:48:57 +0200

    fix: navigation not selecting correct tab

--- src/services/navigation.tsx
+++ src/services/navigation.tsx
@@ -1,3 +1,7 @@
+export function routes(): string[] {
+  return ["/", "/finance", "/on", "/statistics", "/settings"];
+}
+
 export function route2Index(route: string): number {
   const routes: { [key: string]: number } = {
     "/": 0,
@@ -6,6 +10,7 @@ export function route2Index(route: string): number {
     "/statistics": 3,
     "/settings": 4,
   };
+
 
   // Use the type-safe way of accessing object properties
   if (routes.hasOwnProperty(route)) {
